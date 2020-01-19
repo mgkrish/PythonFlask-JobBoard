fd3ad95355fde281ff9e75e9d14c74487e918507
Gopalakrishnan Murthy <mgkrish@users.noreply.github.com>
2020-01-18 23:21:16 -0500

    module 7

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,7 @@
+import datetime
 import sqlite3
 
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, url_for, redirect
 
 PATH = "db/jobs.sqlite"
 
@@ -19,7 +20,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit:
-        connection.commit()
+      results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
     cursor.close()
@@ -54,10 +55,28 @@ def job(job_id):
 @app.route("/employer/<employer_id>")
 def employer(employer_id):
     jobs = execute_sql(
-        'SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?',
+        'SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON '
+        'employer.id = job.employer_id WHERE employer.id = ?',
         [employer_id])
     reviews = execute_sql(
-        'SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?',
+        'SELECT review, rating, title, date, status FROM review JOIN employer ON'
+        ' employer.id = review.employer_id WHERE employer.id = ?',
         [employer_id])
     employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+
+
+@app.route("/employer/<employer_id>/review", methods=["GET", "POST"])
+def review(employer_id):
+    if request.method == "POST":
+        review = request.form["review"]
+        rating = request.form["rating"]
+        title = request.form["title"]
+        status = request.form["status"]
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
+                    (review, rating, title, date, status, employer_id),
+                    commit=True)
+        return redirect(url_for("employer", employer_id=employer_id))
+    else:
+        return render_template("review.html", employer_id=employer_id)
